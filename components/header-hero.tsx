ec2133938af08f19a2bde41220f335432498622a
Aaron Jordan <aaron@impactfulmedia.co>
2025-05-17 09:43:41 -0700

    components

---
+++ components/header-hero.tsx
@@ -0,0 +1,10 @@
+export const HeaderHero = () => (
+  <h1 className="flex flex-col items-center">
+    <span className="font-pixel-display text-5xl text-brand-main">
+      Mazlin Rhea
+    </span>
+    <span className="font-serif uppercase text-4xl text-stone-800 font-bold tracking-widest">
+      Photography
+    </span>
+  </h1>
+);
---
+++ components/navbar.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import { twMerge } from "tailwind-merge";
+
+const navItems = [
+  {
+    href: "/about",
+    label: "Meet Mazlin",
+  },
+  {
+    href: "/photos",
+    label: "Photos",
+  },
+  {
+    href: "/investment",
+    label: "Investment",
+  },
+  {
+    href: "/journal",
+    label: "Journal",
+  },
+  {
+    href: "/contact",
+    label: "Contact",
+  },
+];
+
+const base = "px-3 py-1 rounded-full";
+const hover = "hover:bg-brand-main hover:text-white";
+
+export const Navbar = () => (
+  <ul className="flex justify-center gap-6 font-pixel text-lg uppercase text-brand-main">
+    {navItems.map((target) => (
+      <li key={target.label}>
+        <Link className={twMerge(base, hover)} href={target.href}>
+          {target.label}
+        </Link>
+      </li>
+    ))}
+  </ul>
+);
